232f2225212008eb971489f39f231973e9e64dee
ksenia <park.ksenia23@gmail.com>
2024-03-05 15:14:58 +0600

    add lab6

--- ConsoleApplication1/lab5.cpp
+++ ConsoleApplication1/lab5.cpp
@@ -1,117 +1,117 @@
 //// Task 1
 //
-#include <iostream>
-#include <limits>
-
-class Int {
-private:
-    int value;
-
-public:
-    Int(int val) : value(val) {}
-
-    Int operator+(const Int& other) const {
-        CheckAddOverflow(value, other.value);
-        return Int(value + other.value);
-    }
-
-    Int operator-(const Int& other) const {
-        CheckSubtractOverflow(value, other.value);
-        return Int(value - other.value);
-    }
-
-    Int operator*(const Int& other) const {
-        CheckMultiplyOverflow(value, other.value);
-        return Int(value * other.value);
-    }
-
-    Int operator/(const Int& other) const {
-        if (other.value == 0) {
-            throw std::runtime_error("Division by zero");
-        }
-        return Int(value / other.value);
-    }
-
-    Int& operator++() {  // Префиксная форма
-        CheckIncrementOverflow(value);
-        ++value;
-        return *this;
-    }
-
-    Int operator++(int) {  // Постфиксная форма
-        CheckIncrementOverflow(value);
-        Int temp(value);
-        ++value;
-        return temp;
-    }
-
-    // Вспомогательные функции для проверки переполнения
-    static void CheckAddOverflow(int a, int b) {
-        if ((b > 0 && a > std::numeric_limits<int>::max() - b) ||
-            (b < 0 && a < std::numeric_limits<int>::min() - b)) {
-            throw std::overflow_error("Arithmetic overflow");
-        }
-    }
-
-    static void CheckSubtractOverflow(int a, int b) {
-        if ((b < 0 && a > std::numeric_limits<int>::max() + b) ||
-            (b > 0 && a < std::numeric_limits<int>::min() + b)) {
-            throw std::overflow_error("Arithmetic overflow");
-        }
-    }
-
-    static void CheckMultiplyOverflow(int a, int b) {
-        if (a > 0 && (b > std::numeric_limits<int>::max() / a || b < std::numeric_limits<int>::min() / a) ||
-            a < -1 && (b > std::numeric_limits<int>::min() / a || b < std::numeric_limits<int>::max() / a + 1)) {
-            throw std::overflow_error("Arithmetic overflow");
-        }
-    }
-
-    static void CheckIncrementOverflow(int a) {
-        if ((a == std::numeric_limits<int>::max()) || (a == std::numeric_limits<int>::min())) {
-            throw std::overflow_error("Arithmetic overflow");
-        }
-    }
-
-    // Вывод значения
-    friend std::ostream& operator<<(std::ostream& os, const Int& num) {
-        os << num.value;
-        return os;
-    }
-};
-
-int main() {
-    try {
-        Int a(100);
-        Int b(200);
-
-        // Примеры использования перегруженных операторов
-        Int result1 = a + b;
-        Int result2 = a - b;
-        Int result3 = a * b;
-        Int result4 = a / b;
-
-        Int d(100);
-        Int g = ++a; // Префиксная форма инкремента
-        Int h = d++; 
-
-        std::cout << "a + b = " << result1 << std::endl;
-        std::cout << "a - b = " << result2 << std::endl;
-        std::cout << "a * b = " << result3 << std::endl;
-        std::cout << "a / b = " << result4 << std::endl;
-        std::cout << "a++ = " << g << std::endl;
-        std::cout << "++d = " << h << std::endl;
-
-        Int c(std::numeric_limits<int>::max());
-        c++;  // Попытка увеличить максимальное значение на 1
-
-    }
-    catch (const std::exception& e) {
-        std::cerr << "Exception: " << e.what() << std::endl;
-    }
-
-    return 0;
-}
+//#include <iostream>
+//#include <limits>
+//
+//class Int {
+//private:
+//    int value;
+//
+//public:
+//    Int(int val) : value(val) {}
+//
+//    Int operator+(const Int& other) const {
+//        CheckAddOverflow(value, other.value);
+//        return Int(value + other.value);
+//    }
+//
+//    Int operator-(const Int& other) const {
+//        CheckSubtractOverflow(value, other.value);
+//        return Int(value - other.value);
+//    }
+//
+//    Int operator*(const Int& other) const {
+//        CheckMultiplyOverflow(value, other.value);
+//        return Int(value * other.value);
+//    }
+//
+//    Int operator/(const Int& other) const {
+//        if (other.value == 0) {
+//            throw std::runtime_error("Division by zero");
+//        }
+//        return Int(value / other.value);
+//    }
+//
+//    Int& operator++() {  // Префиксная форма
+//        CheckIncrementOverflow(value);
+//        ++value;
+//        return *this;
+//    }
+//
+//    Int operator++(int) {  // Постфиксная форма
+//        CheckIncrementOverflow(value);
+//        Int temp(value);
+//        ++value;
+//        return temp;
+//    }
+//
+//    // Вспомогательные функции для проверки переполнения
+//    static void CheckAddOverflow(int a, int b) {
+//        if ((b > 0 && a > std::numeric_limits<int>::max() - b) ||
+//            (b < 0 && a < std::numeric_limits<int>::min() - b)) {
+//            throw std::overflow_error("Arithmetic overflow");
+//        }
+//    }
+//
+//    static void CheckSubtractOverflow(int a, int b) {
+//        if ((b < 0 && a > std::numeric_limits<int>::max() + b) ||
+//            (b > 0 && a < std::numeric_limits<int>::min() + b)) {
+//            throw std::overflow_error("Arithmetic overflow");
+//        }
+//    }
+//
+//    static void CheckMultiplyOverflow(int a, int b) {
+//        if (a > 0 && (b > std::numeric_limits<int>::max() / a || b < std::numeric_limits<int>::min() / a) ||
+//            a < -1 && (b > std::numeric_limits<int>::min() / a || b < std::numeric_limits<int>::max() / a + 1)) {
+//            throw std::overflow_error("Arithmetic overflow");
+//        }
+//    }
+//
+//    static void CheckIncrementOverflow(int a) {
+//        if ((a == std::numeric_limits<int>::max()) || (a == std::numeric_limits<int>::min())) {
+//            throw std::overflow_error("Arithmetic overflow");
+//        }
+//    }
+//
+//    // Вывод значения
+//    friend std::ostream& operator<<(std::ostream& os, const Int& num) {
+//        os << num.value;
+//        return os;
+//    }
+//};
+//
+//int main() {
+//    try {
+//        Int a(100);
+//        Int b(200);
+//
+//        // Примеры использования перегруженных операторов
+//        Int result1 = a + b;
+//        Int result2 = a - b;
+//        Int result3 = a * b;
+//        Int result4 = a / b;
+//
+//        Int d(100);
+//        Int g = ++a; // Префиксная форма инкремента
+//        Int h = d++; 
+//
+//        std::cout << "a + b = " << result1 << std::endl;
+//        std::cout << "a - b = " << result2 << std::endl;
+//        std::cout << "a * b = " << result3 << std::endl;
+//        std::cout << "a / b = " << result4 << std::endl;
+//        std::cout << "a++ = " << g << std::endl;
+//        std::cout << "++d = " << h << std::endl;
+//
+//        Int c(std::numeric_limits<int>::max());
+//        c++;  // Попытка увеличить максимальное значение на 1
+//
+//    }
+//    catch (const std::exception& e) {
+//        std::cerr << "Exception: " << e.what() << std::endl;
+//    }
+//
+//    return 0;
+//}
 
 
 
--- ConsoleApplication1/lab6.cpp
+++ ConsoleApplication1/lab6.cpp
@@ -2,7 +2,7 @@
 //#include <string>
 //
 //class Employee {
-//private:
+//protected:
 //    std::string name;
 //    std::string position;
 //    double salary;
@@ -32,7 +32,7 @@
 //    }
 //};
 //
-//class Manager : private Employee {
+//class Manager : public Employee {
 //private:
 //    double annual_bonus;
 //    std::string company_car;
@@ -44,9 +44,9 @@
 //
 //    // Parameterized constructor
 //    Manager(const std::string& empName, const std::string& empPosition, double empSalary,
-//            double bonus, const std::string& car, int options)
+//        double bonus, const std::string& car, int options)
 //        : Employee(empName, empPosition, empSalary), annual_bonus(bonus),
-//          company_car(car), stock_options(options) {}
+//        company_car(car), stock_options(options) {}
 //
 //    // Destructor
 //    ~Manager() {}
@@ -73,3 +73,179 @@
 //
 //    return 0;
 //}
+
+/////////////////////////////////////
+
+// Task 2
+//#include <iostream>
+//#include <cstring>
+//
+//class Display {
+//private:
+//    char type[32];
+//    long colors;
+//    int x_resolution;
+//    int y_resolution;
+//
+//public:
+//    // Parameterized constructor
+//    Display(const char* displayType, long displayColors, int resolutionX, int resolutionY)
+//        : colors(displayColors), x_resolution(resolutionX), y_resolution(resolutionY) {
+//        strcpy_s(type, displayType);
+//    }
+//
+//    const long getColors() const {
+//                return colors;
+//            }
+//
+//    // Display information about the Display
+//    void displayInfo() const {
+//        std::cout << "Display Type: " << type << std::endl;
+//        std::cout << "Colors: " << colors << std::endl;
+//        std::cout << "Resolution: " << x_resolution << "x" << y_resolution << std::endl;
+//    }
+//};
+//
+//class MotherBoard {
+//protected:
+//    int processor;
+//    int speed;
+//    int RAM;
+//
+//public:
+//    // Parameterized constructor
+//    MotherBoard(int processorType, int processorSpeed, int memoryRAM)
+//        : processor(processorType), speed(processorSpeed), RAM(memoryRAM) {}
+//
+//    // Display information about the MotherBoard
+//    void displayInfo() const {
+//        
+//        std::cout << "Processor Type: " << processor << std::endl;
+//        std::cout << "Processor Speed: " << speed << " GHz" << std::endl;
+//        std::cout << "RAM: " << RAM << " GB" << std::endl;
+//    }
+//};
+//
+//class Computer : private Display, public MotherBoard {
+//private:
+//    char name[64];
+//    int hard_disk;
+//
+//public:
+//    // Parameterized constructor
+//    Computer(const char* computerName, const char* displayType, long displayColors,
+//        int resolutionX, int resolutionY, int processorType, int processorSpeed,
+//        int memoryRAM, int diskSize)
+//        : Display(displayType, displayColors, resolutionX, resolutionY),
+//        MotherBoard(processorType, processorSpeed, memoryRAM), hard_disk(diskSize) {
+//        strcpy_s(name, computerName);
+//    }
+//
+//    // Display information about the Computer
+//    void show() const {
+//        std::cout << "COLORS FROM PARENT: " << this->getColors() << std::endl;
+//        std::cout << "Computer Name: " << name << std::endl;
+//        Display::displayInfo();  // Display information from Display
+//        MotherBoard::displayInfo();
+//        std::cout << "Hard Disk Size: " << hard_disk << " GB" << std::endl;
+//    }
+//};
+//
+//int main() {
+//    // Test Computer class
+//    Computer myComputer("Dell", "LED", 16777216, 1366, 768, 1, 3, 8, 512);
+//    myComputer.show();
+//
+//    return 0;
+//}
+
+
+
+
+//////////////////////////
+
+// Task 3
+#include <iostream>
+#include <string>
+
+class Employee {
+private:
+    std::string name;
+   std::string position;
+    double salary;
+
+
+public:
+    // Default constructor
+    Employee() : name(""), position(""), salary(0.0) {}
+
+    // Parameterized constructor
+    Employee(const std::string& empName, const std::string& empPosition, double empSalary)
+        : name(empName), position(empPosition), salary(empSalary) {}
+
+    // Destructor
+    virtual ~Employee() {}
+
+    // Calculate annual salary
+    double calculateAnnualSalary() const {
+        return salary * 12;
+    }
+
+
+    // Display information about the employee
+    void displayInfo() const {
+        std::cout << "Name: " << name << std::endl;
+        std::cout << "Position: " << position << std::endl;
+        std::cout << "Monthly Salary: $" << salary << std::endl;
+        std::cout << "Annual Salary: $" << calculateAnnualSalary() << std::endl;
+    }
+
+    const std::string& getName() const {
+        return name;
+    }
+};
+
+class Manager : private Employee {
+private:
+    double annual_bonus;
+    std::string company_car;
+    int stock_options;
+
+public:
+    // Default constructor
+    Manager() : Employee(), annual_bonus(0.0), company_car(""), stock_options(0) {}
+
+    // Parameterized constructor
+    Manager(const std::string& empName, const std::string& empPosition, double empSalary,
+        double bonus, const std::string& car, int options)
+        : Employee(empName, empPosition, empSalary), annual_bonus(bonus),
+        company_car(car), stock_options(options) {}
+
+    // Destructor
+    ~Manager() {}
+
+    // Display information about the manager
+    void displayManagerInfo() const {
+        displayInfo();  // Inherited from the base class
+        std::cout << "Name FROM MANAGER CLASS: " << this->getName() << std::endl;
+
+        std::cout << "Annual Bonus: $" << annual_bonus << std::endl;
+        std::cout << "Company Car: " << company_car << std::endl;
+        std::cout << "Stock Options: " << stock_options << std::endl;
+    }
+};
+
+int main() {
+    // Test Employee class
+    Employee employee("John Doe", "Developer", 5000.0);
+    employee.displayInfo();
+
+
+    std::cout << "\n";
+
+    // Test Manager class
+    Manager manager("Jane Smith", "Manager", 8000.0, 2000.0, "Company Car", 100);
+    manager.displayManagerInfo();
+   
+    return 0;
+}
